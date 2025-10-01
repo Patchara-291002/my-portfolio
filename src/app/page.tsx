ff1894d8f41555d73f99d9d66d9abd87dbab417e
Patchara Kaewnissai <patchara331003@gmail.com>
2025-10-01 15:13:40 +0700

    rename folder & page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Introduction from "./component/Introduction";
 import TechStack from "./component/TechStack";
-import Project from "./component/project/Project";
-import GetInTouch from "./component/getInTouch/GetInTouch";
+import Project from "./component/project/ProjectPage";
+import GetInTouch from "./component/getInTouch/GetInTouchPage";
 import Footer from "./component/Footer";
 
 export default function Home() {
